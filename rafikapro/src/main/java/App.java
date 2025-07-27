9061bda0871f61e4678b8488468f4cd5c31c5f7a
BlackOwl <sabelo1011@gmail.com>
2025-07-27 20:09:22 +0200

    app entry update

--- rafikapro/src/main/java/App.java
+++ rafikapro/src/main/java/App.java
@@ -1,37 +1,9 @@
 import config.DatabaseConnection;
-import users.role.entity.Role;
-import users.role.sevice.RoleDAOImpl;
-import users.user.entity.Organizer;
-import users.user.entity.User;
-import users.user.entity.Vendor;
-import users.user.reference.UserReferences;
-import users.user.service.organizer_service.OrganizerDaoImpl;
-import users.user.service.user_service.UserDaoImpl;
-import users.user.service.vendor_service.VendorDaoImpl;
 
-import java.time.LocalDateTime;
 
 public class App {
     public static void main(String[] args){
         DatabaseConnection.initialize();
-        RoleDAOImpl roleDAO=new RoleDAOImpl();
-        UserDaoImpl userDao=new UserDaoImpl();
-        OrganizerDaoImpl organizerDAO=new OrganizerDaoImpl();
-        VendorDaoImpl vendorDao=new VendorDaoImpl();
-
-//        User user= userDao.getUserById(5L);
-
-//        Vendor vendor=new Vendor();
-//        vendor.setUser(user);
-//        vendor.setVendorType(UserReferences.vendorType.INDEPENDENT.toString());
-//        vendor.setTradingNumber("AAAA12356");
-//        Vendor newVendor= vendorDao.createVendor(vendor);
-//
-//        System.out.println(newVendor.toString());
-//        Organizer organizer=organizerDAO.getOrganizerById(1L);
-//        System.out.println(organizer.toString());
-//        System.out.println(vendorDao.getVendorById(1L).toString());
-//        vendorDao.getAllVendors().forEach(vendor -> System.out.println(vendor.toString()));
     }
 
 }
